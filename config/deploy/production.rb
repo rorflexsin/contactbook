4bedc4658f5f315f5c382a17d758b594cfe10348
sunil_kumar <sunil_kumar@seologistics.com>
2018-07-04 11:59:52 +0530

    d

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '18.217.146.243', user: 'ubuntu', roles: %w{web app db}
+server '18.217.146.243', user: 'deploy', roles: %w{web app db}
 
 
 # role-based syntax
