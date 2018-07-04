2f546cd2be1e1f9a4eb1d6246528174f5547de41
sunil_kumar <sunil_kumar@seologistics.com>
2018-07-04 12:02:11 +0530

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'contactbook'
 set :repo_url, 'git@github.com:rorflexsin/contactbook.git'
 
 set :branch, :master
-set :deploy_to, '/home/ubuntu/contactbook'
+set :deploy_to, '/home/deploy/contactbook'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
